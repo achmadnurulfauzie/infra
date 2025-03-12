115dfe2403c4a37a576ea486e9d5a31c2cf65e58
Achmad Nurul Fauzie <113338492+achmadnurulfauzie@users.noreply.github.com>
2025-03-12 13:57:06 +0700

    Create docker_install_ubuntu.sh

---
+++ docker_install_ubuntu.sh
@@ -0,0 +1,18 @@
+sudo apt-get update
+sudo apt-get install ca-certificates curl -y
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+docker info
+docker version
+groupadd docker
+sudo usermod -aG docker $USER
+sudo usermod -aG docker ubuntu
+sudo chmod 666 /var/run/docker.sock
+
+docker version
+docker info
